ddf6f0a9c6a30aac0d62eecbbe1ae8198436dbf8
Eric Walti <ewalti@gmail.com>
2015-04-10 08:57:25 -0700

    drop in fastclick

--- ember/addons/ember-material-ui-datetime-picker/assets/ember-material-ui-datetime-picker.js
+++ ember/addons/ember-material-ui-datetime-picker/assets/ember-material-ui-datetime-picker.js
@@ -240,6 +240,23 @@ define('ember-material-ui-datetime-picker/initializers/app-version', ['exports',
   };
 
 });
+define('ember-material-ui-datetime-picker/initializers/ember-cli-fastclick', ['exports', 'ember'], function (exports, Ember) {
+
+  'use strict';
+
+  var EmberCliFastclickInitializer = {
+    name: 'fastclick',
+
+    initialize: function initialize() {
+      Ember['default'].run.schedule('afterRender', function () {
+        FastClick.attach(document.body);
+      });
+    }
+  };
+
+  exports['default'] = EmberCliFastclickInitializer;
+
+});
 define('ember-material-ui-datetime-picker/initializers/ember-moment', ['exports', 'ember-moment/helpers/moment', 'ember-moment/helpers/ago', 'ember-moment/helpers/duration', 'ember'], function (exports, moment, ago, duration, Ember) {
 
   'use strict';
